75bbd93df589f199e7fe9cdc0176ebbdc1265884
Drobyshev Maksim <siberia.maksim@yandex.ru>
2022-03-08 16:10:08 +0700

    Update uploader.js

--- js/uploader.js
+++ js/uploader.js
@@ -1,6 +1,14 @@
-function getJson(url, callback) {
+function getJson(url, callback, errorCallback = null) {
 	const request = new XMLHttpRequest();
 	request.open("GET", url, true);
-	request.onload = callback;
+	request.onload = function() {
+		if (request.status !== 200) {
+			if(errorCallback !== null) {
+				errorCallback(request);
+			}
+		} else {
+			callback(request.responseText);
+		}
+	};
 	request.send();
 }
